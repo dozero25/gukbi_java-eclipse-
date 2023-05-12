6b3bb76e15af616ffab11ec60902dfdf7d79cdd9
dozero25 <ehdud05264@naver.com>
2023-05-12 11:24:23 +0900

    14일차 : 예전에 했던 방식으로 js 재구성, 크게 바뀐 거 없음

--- web/file02/js/login.js
+++ web/file02/js/login.js
@@ -1,23 +1,64 @@
-function login() {
-    let id = document.querySelector("#id");
-    let pw = document.querySelector("#pw");
-    // class => (".id"), id => ("#id")
+// function login() {
+//     let id = document.querySelector("#id");
+//     let pw = document.querySelector("#pw");
+//     // class => (".id"), id => ("#id")
 
-    if(id.value =="") {
-        alert("아이디를 넣어주세요");
-        document.frm.id.focus();
-        return false;
-    } 
+//     if(id.value =="") {
+//         alert("아이디를 넣어주세요");
+//         document.frm.id.focus();
+//         return false;
+//     } 
 
-    if(pw.value =="") {
-        alert("비밀번호를 넣어주세요");
-        document.frm.pw.focus();
-        return false;
+//     if(pw.value =="") {
+//         alert("비밀번호를 넣어주세요");
+//         document.frm.pw.focus();
+//         return false;
+//     }
+
+//     if(id.value == "dozero" && pw.value == "1234") {
+//         alert("로그인 성공");
+//         location.href = "https://www.naver.com";
+//     } 
+
+// }
+
+window.onload = () => {
+    clickEvent.getInstance().onclickEvent();
+}
+
+class clickEvent{
+    static #instance = null;
+    static getInstance() {
+        if(this.#instance == null) {
+            this.#instance = new clickEvent();
+        }
+        return this.#instance;
     }
 
-    if(id.value == "dozero" && pw.value == "1234") {
-        alert("로그인 성공");
-        location.href = "https://www.naver.com";
-    } 
+    onclickEvent() {
+        let id = document.querySelector("#id");
+        let pw = document.querySelector("#pw");
 
+        const button = document.querySelector(".login-button");
+
+        button.onclick = () => {
+            if(id.value =="") {
+                alert("아이디를 넣어주세요");
+                document.frm.id.focus();
+                return false;
+            } 
+
+            if(pw.value =="") {
+                alert("비밀번호를 넣어주세요");
+                document.frm.pw.focus();
+                return false;
+            }
+
+            if(id.value == "dozero" && pw.value == "1234") {
+                alert("로그인 성공");
+                location.href = "https://www.naver.com";
+            } 
+        };
+    }
 }
+
